4332808cfddd684f0c8af2d145753b1bfc789e90
akihisa.kitamura <akihisajyuken@gmail.com>
2020-10-17 11:06:54 +0000

    課題修正1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :update, :edit]
   
   def index
-    if logged_in?
-      @task = current_user.tasks.build  # form_with 用
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
+    @task = current_user.tasks.build  # form_with 用
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -50,7 +48,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = "タスクは正常に削除されました"
-    redirect_back(fallback_location: root_url)
+    redirect_to root_url
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
   get 'signup', to: 'users#new'
-  
-  get 'new', to: 'tasks#new'
-  get 'show', to: 'tasks#show'
-  
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   resources :tasks
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
 end
